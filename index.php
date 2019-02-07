41ccfa96a9099c7ad6254637bf1e568958dc0991
Ari irawan <a.irawann96@gmail.com>
2019-02-07 21:38:22 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,13 +12,13 @@ for ($i=1; $i <= 100; $i++) {
     # code...
     $a = $i/$_a; $b = $i/$_b;
     
-    if(is_int($a)==true and is_float($b)==true){
+    if(is_int($a)==true and is_float($b)==true){//$i bisa dibagi 3 dan $i tidak bisa dibagi 5
         $_return = '<span class="red">apa</span>, ';
-    }elseif(is_int($b)==true and is_float($a)==true){
+    }elseif(is_int($b)==true and is_float($a)==true){ //$i bisa dibagi 5 dan $i tidak bisa dibagi 3
         $_return = 'Bole, ';    
-    }elseif(is_int($a)==true and is_int($b)==true ){
+    }elseif(is_int($a)==true and is_int($b)==true ){ //$i tidak bisa dibagi 3 dan $i tidak bisa dibagi 5
         $_return ='<span class="green"> ApaBole</span>, ';
-    }elseif(is_float($a)==true or is_float($b)==true ){
+    }elseif(is_float($a)==true or is_float($b)==true ){ //$i bisa dibagi 3 dan 5
         $_return = $i.', ';
     }
 
